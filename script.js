f81c6b61feb7299434e4e56d2cdb9745b9cf3786
EVG230 <67220117+EVG230@users.noreply.github.com>
2021-03-03 20:54:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
+function compute() {
     p = document.getElementById("principal").value;
-    
+
 }
-        
+var principal = document.getElementById("principal")
+var rate = document.getElementById("rate")
+var years = document.getElementById("years")
+if (years = ! "") {
+    if (years = 1) {
+
+    }
+}
+var interest = document.getElementById("principal * years *rate / 100")
+//Convert "No of Years" into the actual year in the future
+
+
+function read() {
+    r = document.getElementById("interest").value
+    return r
+
+}
